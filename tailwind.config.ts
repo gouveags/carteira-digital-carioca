34415f98f82dd7cdde8f11e6ca7ec0b3115f876e
Gabriel Gouvea <gabriel.silva@ip.tv>
2025-01-14 23:59:03 -0300

    Fix import formatting

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from 'tailwindcss'
-import tailwindcssAnimate from 'tailwindcss-animate';
+import tailwindcssAnimate from 'tailwindcss-animate'
 
 const config = {
   darkMode: ['class'],
