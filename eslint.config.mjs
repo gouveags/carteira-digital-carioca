43a77821bd52547e06d3583c6fd0eb51c8cb84b4
Gabriel Gouvea <gabriel.silva@ip.tv>
2025-01-14 23:49:23 -0300

    Update eslint.config.mjs to fit civitas-app pattern

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,19 +1,27 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
-});
+})
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    extends: ["next", "prettier"],
-  },
-];
+  ...compat.extends(
+    'plugin:@next/next/recommended',
+    '@rocketseat/eslint-config/next'
+  ),
+  ...compat.config({
+    plugins: ['simple-import-sort', 'react'],
+    rules: {
+      'simple-import-sort/imports': 'error',
+      'import/newline-after-import': 'error',
+      'react/jsx-key': 'error',
+    },
+  }),
+]
 
-export default eslintConfig;
+export default eslintConfig
